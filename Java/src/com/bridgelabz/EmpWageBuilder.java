c7de40cb9b260a11008d6e62b5f1abc7ac20a2bc
sid-thakur47 <sidthakur258@gmail.com>
2020-04-16 18:21:27 +0530

    ADDED:Part time Employee feature

--- Java/src/com/bridgelabz/EmpWageBuilder.java
+++ Java/src/com/bridgelabz/EmpWageBuilder.java
@@ -2,20 +2,25 @@ package com.bridgelabz;
 
 public class EmpWageBuilder {
     private static final int IS_FULL_TIME = 1;
+    private static final int IS_PART_TIME = 2;
     private static final int EMP_RATE_PER_HOUR = 20;
 
     public static void main(String args[]) {
+        int empHrs;
+        int empWage;
 
-        int empHrs = 0;
-        int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10 % 2);
-        if (empCheck == IS_FULL_TIME) {
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_PART_TIME) {
+            empHrs = 4;
+        } else if (empCheck == IS_FULL_TIME) {
             empHrs = 8;
         } else {
             empHrs = 0;
         }
+
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage is:" + empWage);
+
     }
 
 }
