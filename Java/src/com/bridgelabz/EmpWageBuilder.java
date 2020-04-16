7f6255219c3e56e2d248abf416714305368e87c6
sid-thakur47 <sidthakur258@gmail.com>
2020-04-16 19:33:57 +0530

    ADDED:Function to compute Employee wage

--- Java/src/com/bridgelabz/EmpWageBuilder.java
+++ Java/src/com/bridgelabz/EmpWageBuilder.java
@@ -1,14 +1,28 @@
 package com.bridgelabz;
 
-public class EmpWageBuilder {
+public class EmpWageBuilder11 {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int MAX_HR_IN_MONTH = 100;
     public static final int NUM_WORKING_DAYS = 20;
     public static final int EMP_RATE_PER_HOUR = 20;
 
-    public static void main(String[] args) {
-
+    private static int getWorkingHours() {
+        int empHrs;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        return empHrs;
+    }
+    public EmpWageBuilder11() {
         int totalEmpHour = 0;
         int totalEmpWage = 0;
         int totalWorkingDays = 0;
@@ -16,18 +30,8 @@ public class EmpWageBuilder {
         while (totalEmpHour <= MAX_HR_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
             totalWorkingDays++;
             int empWage = 0;
-            int empHrs;
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empCheck) {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-            }
+            int empHrs = getWorkingHours();
+
             empWage = empHrs * EMP_RATE_PER_HOUR;
             totalEmpWage = totalEmpWage + empWage;
             totalEmpHour = totalEmpHour + empHrs;
@@ -35,5 +39,8 @@ public class EmpWageBuilder {
         }
         System.out.println("Total Employee wage is:" + totalEmpWage);
         System.out.println("Total Employee Hours is:" + totalEmpHour);
+    }
+    public static void main(String[] args) {
+        EmpWageBuilder ewb = new EmpWageBuilder();
     }
 }
