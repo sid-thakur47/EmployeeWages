5f125ee248cddd4081817ecb4530d06d7458ccba
sid-thakur47 <sidthakur258@gmail.com>
2020-04-16 18:44:28 +0530

    ADDED:Functionality to calculate wage unit certain days/hours

--- Java/src/com/bridgelabz/EmpWageBuilder.java
+++ Java/src/com/bridgelabz/EmpWageBuilder.java
@@ -3,15 +3,20 @@ package com.bridgelabz;
 public class EmpWageBuilder {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
+    public static final int MAX_HR_IN_MONTH = 100;
+    public static final int NUM_WORKING_DAYS = 20;
     public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int WORKING_DAYS_PER_MONTH = 20;
 
     public static void main(String[] args) {
-        int empHrs;
-        int empWage = 0;
+
+        int totalEmpHour = 0;
         int totalEmpWage = 0;
-        int day;
-        for (day = 0; day <= WORKING_DAYS_PER_MONTH; day++) {
+        int totalWorkingDays = 0;
+
+        while (totalEmpHour <= MAX_HR_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
+            totalWorkingDays++;
+            int empWage = 0;
+            int empHrs;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_PART_TIME:
@@ -25,8 +30,10 @@ public class EmpWageBuilder {
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             totalEmpWage = totalEmpWage + empWage;
+            totalEmpHour = totalEmpHour + empHrs;
             System.out.println("Employee Wage is:" + empWage);
         }
         System.out.println("Total Employee wage is:" + totalEmpWage);
+        System.out.println("Total Employee Hours is:" + totalEmpHour);
     }
 }
