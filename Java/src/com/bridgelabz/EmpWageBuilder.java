9fa05eb615c3373711cd4eb91174a4f41ee0cc45
sid-thakur47 <sidthakur258@gmail.com>
2020-04-16 18:01:36 +0530

    ADDED:Functionality to check employee attendence

---
+++ Java/src/com/bridgelabz/EmpWageBuilder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class EmpWageBuilder {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10 % 2);
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+    }
+}
