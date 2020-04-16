db5d831917ca29fefc72331eb0ec21e83bdb04fe
sid-thakur47 <sidthakur258@gmail.com>
2020-04-16 18:28:11 +0530

    ADDED:Implemented using case statement

--- Java/src/com/bridgelabz/EmpWageBuilder.java
+++ Java/src/com/bridgelabz/EmpWageBuilder.java
@@ -1,26 +1,25 @@
 package com.bridgelabz;
 
 public class EmpWageBuilder {
-    private static final int IS_FULL_TIME = 1;
-    private static final int IS_PART_TIME = 2;
-    private static final int EMP_RATE_PER_HOUR = 20;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
 
-    public static void main(String args[]) {
-        int empHrs;
-        int empWage;
-
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_PART_TIME) {
-            empHrs = 4;
-        } else if (empCheck == IS_FULL_TIME) {
-            empHrs = 8;
-        } else {
-            empHrs = 0;
+    public static void main(String[] args) {
+        int empHrs = 0;
+        int empWage = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
         }
-
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage is:" + empWage);
-
     }
-
 }
