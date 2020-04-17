c059310041ef8828cf99df322d2a47da27b4eef2
sid-thakur47 <sidthakur258@gmail.com>
2020-04-17 14:07:14 +0530

    ADDED:Deleted file

---
+++ Java/src/com/bridgelabz/EmpWageBuilder.java
@@ -0,0 +1,43 @@
+package com.bridgelabz;
+
+public class EmpWageBuilder {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+
+    private static int getWorkingHours() {
+        int empHrs;
+        int empCheck = (int) Math.floor(Math.random() * 100) % 3;
+        switch (empCheck) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        return empHrs;
+    }
+
+    public EmpWageBuilder() {
+        computeEmployeeWage("Birla", 50, 20, 100);
+        computeEmployeeWage("Tata", 60, 40, 200);
+    }
+
+    public void computeEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHourInMonth) {
+        int totalWorkHours = 0;
+        int totalEmpWage = 0;
+        int totalWorkingDays = 0;
+        while (totalWorkHours <= maxHourInMonth && totalWorkingDays < numOfWorkingDays) {
+            totalWorkingDays++;
+            totalWorkHours = totalWorkHours + getWorkingHours();
+        }
+        totalEmpWage = totalWorkHours * empRatePerHour;
+        System.out.println("Total Employee wage of " + company + " is:" + totalEmpWage);
+    }
+
+    public static void main(String[] args) {
+        new EmpWageBuilder();
+    }
+}
